69bc51ac4ba951a0093e2e9fcdb01f30990f0b82
zhangzw218 <745787360@qq.com>
2018-07-08 23:52:08 +0800

    第一次提交

---
+++ SCB/Controllers/StudyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCB.Controllers
+{
+    public class StudyController : Controller
+    {
+        // GET: Study
+        public ActionResult Index()
+        {
+            return Html();
+        }
+        // GET: Study
+        public ActionResult Html()
+        {
+            return View();
+        }
+        // GET: Study
+        public ActionResult Css()
+        {
+            return View();
+        }
+        // GET: Study
+        public ActionResult JavaScrpts()
+        {
+            return View();
+        }
+    }
+}
